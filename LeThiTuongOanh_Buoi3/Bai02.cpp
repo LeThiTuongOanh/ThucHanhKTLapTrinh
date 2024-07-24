76f1f1cb24c5a34d011e6dad85410b173af6e989
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-07-24 22:15:17 +0700

    Buoi03_Bai2_Cau3: Xuat cac phan tu thuoc duong cheo song song voi duong cheo chinh

--- LeThiTuongOanh_Buoi3/Bai02.cpp
+++ LeThiTuongOanh_Buoi3/Bai02.cpp
@@ -35,11 +35,33 @@ void xuatCacPhanTuTrenDuongCheoChinh(int a[][50], int n)
 		}											
 	}
 }
+
+void xuatDuongCheoSongSong(int a[][50], int n)
+{
+	printf("Duong cheo song song phia tren duong cheo chinh:\n");
+	for (int k = 1; k < n-1; k++) {
+		for (int i = 0; i < n - k; i++) {
+			printf("%4d", a[i][i + k]);
+		}
+		printf("\n");
+	}
+
+	printf("Duong cheo song song phia duoi duong cheo chinh:\n");
+	for (int k = 1; k < n-1; k++) {
+		for (int i = k; i < n; i++) {
+			printf("%4d", a[i][i - k]);
+		}
+		printf("\n");
+	}
+
+
+}
 void Menu()
 {
 	printf("\n------Menu---------\n");
 	printf("1. Tao ma tran vuong cap n>5.\n ");
 	printf("2. Xuat cac phan tu tren duong cheo chinh.\n");
+	printf("3. Xuat cac phan tu thuoc duong cheo song song voi duong cheo chinh.\n");
 }
 int main()
 {
@@ -63,6 +85,10 @@ int main()
 			case 2:
 				printf("Cac phan tu tren duong cheo chinh: ");
 				xuatCacPhanTuTrenDuongCheoChinh(a, n);
+				break;
+			case 3:
+				xuatDuongCheoSongSong(a, n);
+				break;
 			default: 
 				break;
 			}
