2c827ab70fd7c76ca66306c6b69f7d4005215b2a
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-07-24 14:43:29 +0700

    Buoi03_Bai1_2

--- LeThiTuongOanh_Buoi3/Bai01.cpp
+++ LeThiTuongOanh_Buoi3/Bai01.cpp
@@ -21,6 +21,22 @@ void xuat_ma_tran(int m, int n, int ma_tran[][50]) {
 }
 
 
+void tong_tung_dong(int m, int n, int ma_tran[][50], int tong[50]) {
+	for (int i = 0; i < m; i++) {
+		tong[i] = 0;
+		for (int j = 0; j < n; j++) {
+			tong[i] += ma_tran[i][j];
+		}
+	}
+}
+
+void xuat_tong_tung_dong(int m, int tong[50]) {
+	for (int i = 0; i < m; i++) {
+		printf("Tong dong %d: %d\n", i, tong[i]);
+	}
+}
+
+
 
 int main() {
 	int m,n, k = 10;
@@ -39,7 +55,7 @@ int main() {
 		printf("1. Tao va xuat ma tran \n");
 		printf("2. Tinh tong gia tri tren tung dong\n");
 		
-		printf("0. Thoat\n");
+		printf(" Thoat\n");
 		printf("Nhap lua chon cua ban ");
 		scanf_s("%d", &choice);
 
@@ -49,7 +65,11 @@ int main() {
 			printf("Ma tran ngau nhien:\n");
 			xuat_ma_tran(m, n, ma_tran);
 			break;
-	
+		case 2:
+			tong_tung_dong(m, n, ma_tran, tong);
+			printf("Tong gia tri tung dong:\n");
+			xuat_tong_tung_dong(m, tong);
+			break;
 	
 		case 0:
 			break;
