501f7f03d784c18ff326a8e94cd2faefd4672ec0
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-08-05 16:47:25 +0700

    Buoi4_DeQuy: Bai 1_3 : de quy va khu de quy

--- LeThiTuongOanh_Buoi4/Bai1.cpp
+++ LeThiTuongOanh_Buoi4/Bai1.cpp
@@ -16,7 +16,19 @@ int tong_day_khu_de_quy(int n) {
 	}
 	return tong;
 }
+double tong_phan_so_de_quy(int n) {
+	if (n == 1)
+		return 1.0 / 2;
+	return (double)n / (n + 1) + tong_phan_so_de_quy(n - 1);
+}
 
+double tong_phan_so_khu_de_quy(int n) {
+	double tong = 0;
+	for (int i = 1; i <= n; i++) {
+		tong += (double)i / (i + 1);
+	}
+	return tong;
+}
 
 int main() {
 	int n;
@@ -25,6 +37,9 @@ int main() {
 
 	printf("Tong day 1 + 2 + ... + %d (De quy): %d\n", n, tong_day_de_quy(n));
 	printf("Tong day 1 + 2 + ... + %d (Khu de quy): %d\n", n, tong_day_khu_de_quy(n));
+
+	printf("Tong 1/2 + 2/3 + ... + %d/(%d+1) (De quy): %f\n", n, n, tong_phan_so_de_quy(n));
+	printf("Tong 1/2 + 2/3 + ... + %d/(%d+1) (Khu de quy): %f\n", n, n, tong_phan_so_khu_de_quy(n));
 
 	getchar();
 	getchar();
