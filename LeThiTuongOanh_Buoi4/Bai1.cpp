483d977d101333a08664dc5523c5a59299b9e8c1
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-08-05 16:28:48 +0700

    Buoi4_DeQuy:Bai1_1

---
+++ LeThiTuongOanh_Buoi4/Bai1.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+// Hàm tính tổng 1 + 2 + 3 + ... + n (Đệ quy)
+int tong_day_de_quy(int n) {
+	if (n == 1)
+		return 1;
+	return n + tong_day_de_quy(n - 1);
+}
+
+// Hàm tính tổng 1 + 2 + 3 + ... + n (Khử đệ quy)
+/*int tong_day_khu_de_quy(int n) {
+	int tong = 0;
+	for (int i = 1; i <= n; i++) {
+		tong += i;
+	}
+	return tong;
+}
+*/
+
+int main() {
+	int n;
+	printf("Nhap vao so nguyen duong n: ");
+	scanf_s("%d", &n);
+
+	printf("Tong day 1 + 2 + ... + %d (De quy): %d\n", n, tong_day_de_quy(n));
+//	printf("Tong day 1 + 2 + ... + %d (Khu de quy): %d\n", n, tong_day_khu_de_quy(n));
+
+	getchar();
+	getchar();
+	return 0;
+}
